12496e0ef56ac7a2d98a6a57fdab7e89ce07eef3
jjamster <36107455+jjamster@users.noreply.github.com>
2026-04-13 16:21:46 -0400

    Phyto PUSH

--- phytoPlank.m
+++ phytoPlank.m
@@ -1,4 +1,25 @@
+%%
+clear;
 %% For the csv file
 
 filename = 'spots.csv'
 SPOTS = readtable(filename);
+%%
+latitude = SPOTS.LATITUDE;
+longitude = SPOTS.LONGITUDE;
+
+oxygen = SPOTS.CTDOXY;
+nitrate = SPOTS.NITRAT;
+pCO2 = SPOTS.PCO2;
+DOC = SPOTS.TCARBN;
+
+%look at documentation for time
+% look at total carbon or pCO2 -> dissolved carbon
+% is most related to what we do
+
+%%
+
+
+
+
+
