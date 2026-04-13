d108c6ef0632aa530cc70675e6cf71e66932b98b
jjamster <36107455+jjamster@users.noreply.github.com>
2026-04-13 16:04:21 -0400

    Pushing IRON

--- phytoPlank.m
+++ phytoPlank.m
@@ -1,5 +1,22 @@
 %% Hello 
 
-filename = 3+3;
+filenames = ["Monthly_dFe_V2.nc"];
+
+for i = 1:length(filenames)
+  ncdisp(filenames(i));
+end
+
+lat = ncread(filenames, "Latitude");
+lon = ncread(filenames, "Longitude");
+
+%% 1c. Use the function "ncread" to extract the variables "time" and
+%"ctdmo_seawater_temperature"
+iron = ncread(filenames, "dFe_RF");
+months = ncread(filenames, "Month")
+
+figure(1);
+plot(months, iron, "k.")
+datetick("x", 22)
+
 
 %%
